aca9fe0a090039918a9394e23778fbcda5255d85
Adrian Burlacu <adrian.burlacu@live.com>
2021-08-21 18:07:27 +0300

    Enable LAN. Phone works ;)

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
   "STARK_MODES": "[false, false, true]",
-  "STARK_HOST": "localhost",
+  "STARK_HOST": "192.168.0.73",
   "STARK_PORT": "3005",
   "STARK_DB_HOST": "192.168.0.73",
   "STARK_USER_NAME": "admin",
   "STARK_USER_PASSWORD": "dmW8EIdkKnT4hFW0WZhs",
-  "STARK_USER_KEY": "pCSLvXBF4T"
+  "STARK_USER_KEY": "hndvr40Vk6"
 }
