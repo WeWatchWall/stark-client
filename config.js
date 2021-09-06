b1d471626bc829a2ee00ed358d3c39f591acf6da
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-06 13:40:31 +0300

    Fix config.js.

--- config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "STARK_MODES": "[false, false, true]",
-  "STARK_HOST": "192.168.0.73",
-  "STARK_PORT": "3005",
-  "STARK_DB_HOST": "192.168.0.73",
-  "STARK_USER_NAME": "admin",
-  "STARK_USER_PASSWORD": "dmW8EIdkKnT4hFW0WZhs",
-  "STARK_USER_KEY": "kM7clXOLL8"
-}
